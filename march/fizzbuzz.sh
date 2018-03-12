818731198631a8d6640450901dfd890eefd10825
santorazz8 <santorazz8@gmail.com>
2018-03-12 14:02:48 -0400

    after 1 month

---
+++ march/fizzbuzz.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# we will work to see if we can play fizzbuzz
+
+for num in range (1,100):
+    if num % 3 == 0: and num % 5 == 0:
+    echo ('fizzbuzz')
+   elseif num % 3 == 0:
+    echo ('fizz')
+    elseif num % 5 == 0:
+    echo ('buzz')
+    else
+    echo ('num')
+
+fi
+    
