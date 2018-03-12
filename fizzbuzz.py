f9ed6e0164a5b70c43a65371935103d3f9024e80
santorazz8 <santorazz8@gmail.com>
2018-03-12 14:36:17 -0400

    this is python

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# we will work to see if we can play fizzbuzz
+
+for num in range (1,100):
+    if num % 3 == 0  and num % 5 == 0:
+           print ('fizzbuzz')
+    elif num % 3 == 0:
+           print ('fizz')
+    elif num % 5 == 0:
+           print ('buzz')
+    else:
+           print ('num')
+
+    
