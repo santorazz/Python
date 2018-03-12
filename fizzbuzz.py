74982114189cb57ede653f04a4878e7241efb459
santorazz <35788839+santorazz@users.noreply.github.com>
2018-03-12 17:15:17 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # we will work to see if we can play fizzbuzz
 
-for num in range (1,100):
+for num in range (1,inf):
     if num % 3 == 0  and num % 5 == 0:
            print ('fizzbuzz')
     elif num % 3 == 0:
